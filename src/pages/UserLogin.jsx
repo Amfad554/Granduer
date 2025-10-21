0cda3aaea0e365a9aa189e85d9279276fa3af49e
Favour <georgechiamaka02@gmail.com>
2025-10-21 12:21:56 -0700

    added verceljson file2

--- src/pages/UserLogin.jsx
+++ src/pages/UserLogin.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { FiMail, FiLock, FiUser, FiPhone } from "react-icons/fi";
 
-import Input from "../shared/Input";
 import Layout from "../shared/Layout";
+import Input from "../shared/Input";
 
 const UserLoginPage = () => {
   const [isLogin, setIsLogin] = useState(true);
