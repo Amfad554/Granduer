e275eb6f14cec9257d17a5a8620f1feef1881957
Favour <georgechiamaka02@gmail.com>
2025-10-21 12:35:42 -0700

    added verceljson file6

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,6 +32,7 @@ import Layout from "../shared/Layout";
 
 
 
+
 const Home = () => {
   const { HandleGetProducts, productData, HandleAddTCart } = useContext(ProductContext);
   const [bestSeller, setBestSeller] = useState([]);
